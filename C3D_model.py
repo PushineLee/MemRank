a75584bfe149e9941519ebe15b19f334afaa7642
PushineLee <liyh@bupt.edu.cn>
2023-06-12 12:45:36 +0800

    add model&weight

---
+++ C3D_model.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class C3D(nn.Module):
+    """
+    The C3D network.
+    """
+
+    def __init__(self, use_feature=False, fds=False, use_buffer = True, cls = 46):
+        super(C3D, self).__init__()
+
+        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+
+        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+
+        self.fc6 = nn.Linear(8192, 4096)
+        self.fc7 = nn.Linear(4096, 64)
+        self.fc8 = nn.Linear(64, 1)
+
+        self.dropout = nn.Dropout(p=0.5)
+        self.relu = nn.ReLU()       
+
+    def forward(self, x, epoch,gate, targets=None):
+
+        x = self.relu(self.conv1(x))
+        x = self.pool1(x)
+
+        x = self.relu(self.conv2(x))
+        x = self.pool2(x)
+
+        x = self.relu(self.conv3a(x))
+        x = self.relu(self.conv3b(x))
+        x = self.pool3(x)
+
+        x = self.relu(self.conv4a(x))
+        x = self.relu(self.conv4b(x))
+        x = self.pool4(x)
+
+        x = self.relu(self.conv5a(x))
+        x = self.relu(self.conv5b(x))
+        x = self.pool5(x)
+
+        
+        x = x.view(-1, 8192)
+        feature = x
+
+        x = self.relu(self.fc6(x))
+        x = self.dropout(x)
+        x = self.fc7(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc8(x)
+
+        return x.squeeze(-1),feature
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MemRank
 Memory-Augmented Similarity Ranking for Depression Recognition
+model weight [[Baidu Netdisk]](https://pan.baidu.com/s/1o38G1CKp_uwZ3nXCyToTSQ?pwd=hpos) for C3D backbone on AVEC 2014.
+model weight [[Baidu Netdisk]](https://pan.baidu.com/s/1BwR1fnqny8_ONjjz4XIS5g?pwd=g8aw) for MemRank on AVEC 2014.
